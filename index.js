4fcfee883837f9284d8dcb64c1e190d687aa5357
vince94 <haxxor199x@gmail.com>
2019-10-12 20:31:06 -0400

    Fix async/await stuff, parameters

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
+const displayInConsole = true;
 
-async function displayPrice() {
+async function displayPrice(cryptoName, dateToCheck) {
     try {
-        const data = await CoinGeckoClient.coins.fetchHistory('bitcoin', {
-            date: '25-9-2017'
-        });
-        // price = data.market_data.current_price
-        console.log(data.data.market_data.current_price.usd)
-    }
+        var data = await CoinGeckoClient.coins.fetchHistory(cryptoName, { date: dateToCheck });
+        return data.data.market_data.current_price.usd;
+        }
     catch (err) {
-        console.log('Error, err.message')
+        console.error(err.message)
     }
 };
-displayPrice();
 
-async function displayChart() {
+if (displayInConsole) {
+	//(async () => {
+	//	const foo = await displayPrice('bitcoin', '25-9-2017');
+	//	console.log(foo);
+	//})();
+}
+
+
+async function displayChart(cryptoName, daysToDisplay) {
     try {
-        const chart = await CoinGeckoClient.coins.fetchMarketChart('bitcoin', { days: '1' });
+        const chart = await CoinGeckoClient.coins.fetchMarketChart(cryptoName, {days:daysToDisplay});
         price = chart.data.prices
-        price.forEach(price => console.log(price[0]))
-    }
+		return price;
+        //price.forEach(price => console.log(price[0]))
+        }
     //date is in unix timestamp
     catch (err) {
-        console.log('Error, err.message')
+        console.error(err.message)
     }
 };
-displayChart();
 
-async function convertTimestamptoTime() {
-    unixTimestamp = 1569359448729;
-    dateObj = new Date(unixTimestamp * 1000);
-    utcString = dateObj.toUTCString();
-
-    time = utcString;
-    console.log(time)
-}
-convertTimestamptoTime();
-
-async function getUnixTimstamp() {
-    timestamp = 'Sat, 08 Jan 51701 12:12:09 GMT'
-    Date.prototype.getUnixTime = function () { return this.getTime() / 1000 };
-    const parsedUnixTime = new Date(timestamp).getUnixTime();
-    console.log(parsedUnixTime)
-}
-getUnixTimstamp();
-
-const closestTimeStamp = function(array, num) {
-    var i = 0;
-    var minDiff = 1000;
-    var result;
-    for (i in array) {
-        var m = Math.abs(num - array[i]);
-        if (m < minDiff) {
-            minDiff = m;
-            result = array[i];
-        }
-    }
-    return result;
+if (displayInConsole) {
+	//(async () => {
+	//	const foo = await displayChart('bitcoin', '1');
+	//	console.log(foo);
+	//})();
 }
 
-module.exports = {
-    closestTimeStamp
+
+async function unixTimestampToString(unixTimestamp) {
+	originalDate = new Date(unixTimestamp * 1000);
+	return originalDate.toUTCString();
 };
+
+if (displayInConsole) {
+	//(async () => {
+	//	const foo = await unixTimestampToString(1569359448729);
+	//	console.log(foo)
+	//})();
+}
+
+
+function stringToUnixTimestamp(stringTimestamp) {
+    Date.prototype.getUnixTime = function() { return this.getTime()/1000 };
+	return new Date(stringTimestamp).getUnixTime();
+}
+
+if (displayInConsole) {
+	//console.log(stringToUnixTimestamp('Sat, 08 Jan 51701 12:12:09 GMT'));
+}
+
+
+function closestTimeStamp(array, num) {
+  //https://stackoverflow.com/questions/8584902/get-closest-number-out-of-array
+  var i = 0;
+  var minDiff = 1000;
+  var result;
+  for (i in array) {
+    var m = Math.abs(num - array[i]);
+    if (m < minDiff) {
+      minDiff = m;
+      result = array[i];
+    }
+  }
+  return result;
+}
+
+if (displayInConsole) {
+	//const testArray = [3, 4, 7, 9];
+	//console.log(closestTimeStamp(testArray), 5); //is this array passed in right?
+}
